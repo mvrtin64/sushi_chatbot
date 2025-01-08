8435eb6c79e7714c396e945b119cb15313bf20b5
mvrtin64 <mvrtingrzsk@gmail.com>
2025-01-08 11:36:50 -0300

    minor changes

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  // email: { type: String, unique: true },
+  name: { type: String, required: true }
 });
 
 module.exports = mongoose.model('users', userSchema, 'users');
